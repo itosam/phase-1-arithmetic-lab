0aa0b7ca6bad641743a81f403a41b752c6bab1ad
Osamu Ito <mr.samito@gmail.com>
2022-08-28 21:23:52 -0400

    commit tree 083e0b294a1ec72c53a73d6cf07542bfe6a7304d

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 100;
+let num4 = 2;
+let mod = Number.parseInt(num3,num4);
+
+let max = Math.max(5,12,20);
